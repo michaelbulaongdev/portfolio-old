5328e128fadff42edf1c68d042aef5eef384f472
michaelbulaongdev <michaelbulaongdev@gmail.com>
2023-02-24 09:46:46 +0800

    map for works

--- src/components/Works.js
+++ src/components/Works.js
@@ -10,6 +10,7 @@ import {
   Link,
   Typography
 } from "@mui/material";
+import listWorks from "./listWorks";
 
 const Works = () => {
   return (
@@ -18,81 +19,34 @@ const Works = () => {
         Sample Works
       </Typography>
       <Grid my={3} container spacing={2} sx={{alignItems: 'stretch'}}>
-        <Grid item xs={12} md={4}>
-          <Card>
-            <CardMedia
-              sx={{minHeight: 200}}
-              image='https://i.imgur.com/73YDKHQ.png'
-              title='pokedex thumbnail'
-            />
-            <CardContent>
-              <Typography variant='h6' component='div'>
-                Basic Pokedex
-              </Typography>
-              <Typography variant='subtitle2' color='text.secondary'>
-                project working with fetching and parsing APIs
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button
-                href='https://michaelbulaongdev.github.io/project010-pokedex-project/'
-                target='_blank'
-                rel='noopener'
-                size='small'
-              >
-                visit site
-              </Button>
-            </CardActions>
-          </Card>
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <Card>
-            <CardMedia
-              sx={{minHeight: 200}}
-              image='https://i.imgur.com/qwxnw3a.png'
-              title='simon game thumbnail'
-            />
-            <CardContent>
-              <Typography variant='h6' component='div'>
-                Simon Game
-              </Typography>
-              <Typography variant='subtitle2' color='text.secondary'>
-                project about game logic, audio media, and event listeners
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button
-                href='https://michaelbulaongdev.github.io/Project003-simon-game/'
-                target='_blank'
-                rel='noopener'
-                size='small'>visit site</Button>
-            </CardActions>
-          </Card>
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <Card>
-            <CardMedia
-              sx={{minHeight: 200}}
-              image='https://i.imgur.com/zm4SdZN.png'
-              title='stats preview thumbnail'
-            />
-            <CardContent>
-              <Typography variant='h6' component='div'>
-                Stats Preview Card
-              </Typography>
-              <Typography variant='subtitle2' color='text.secondary'>
-                project about CSS attributes and text/image manipulation
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button
-                href='https://michaelbulaongdev.github.io/FM005-stats-preview-card/'
-                target='_blank'
-                rel='noopener'
-                size='small'>visit site</Button>
-            </CardActions>
-          </Card>
-        </Grid>
+        {listWorks.map((item) => (
+          <Grid key={item.link} item xs={12} md={4}>
+            <Card>
+              <CardMedia
+                sx={{minHeight: 200}}
+                image={item.image}
+                title={item.thumbnail}
+              />
+              <CardContent>
+                <Typography variant='h6' component='div'>
+                  {item.title}
+                </Typography>
+                <Typography variant='subtitle2' color='text.secondary'>
+                  {item.description}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button
+                  href={item.link}
+                  target='_blank'
+                  rel='noopener'
+                  size='small'
+                > visit site
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
       </Grid>
       <Typography variant='body2'>
         For more samples, please visit my <Link color='primary.dark' target='_blank' href='https://github.com/michaelbulaongdev'>GitHub account page</Link>.
---
+++ src/components/listWorks.js
@@ -0,0 +1,46 @@
+const listWorks = [
+  {
+    image: 'https://i.imgur.com/73YDKHQ.png',
+    thumbnail: 'pokedex thumbnail',
+    title: 'Basic Pokedex',
+    description: 'project on fetching and parsing API with React.js',
+    link: 'https://michaelbulaongdev.github.io/project010-pokedex-project/'
+  },
+  {
+    image: 'https://i.imgur.com/qwxnw3a.png',
+    thumbnail: 'simon game thumbnail',
+    title: 'Simon Game',
+    description: 'game logic, audio media, and event listeners - HTML/CSS/JS',
+    link: 'https://michaelbulaongdev.github.io/Project003-simon-game/'
+  },
+  {
+    image: 'https://i.imgur.com/RwWm3D5.png',
+    thumbnail: 'stats preview thumbnail',
+    title: 'Stats Preview Card',
+    description: 'frontend mentor challenge, HTML/CSS',
+    link: 'https://michaelbulaongdev.github.io/FM005-stats-preview-card/'
+  },
+  {
+    image: 'https://i.imgur.com/9GVeAgc.png',
+    thumbnail: '3-column preview thumbnail',
+    title: '3-Column Preview',
+    description: 'frontend mentor challenge, HTML/CSS',
+    link: 'https://michaelbulaongdev.github.io/FM006-3-column-preview/'
+  },
+  {
+    image: 'https://i.imgur.com/O5zLpRG.png',
+    thumbnail: 'order summary thumbnail',
+    title: 'Order Summary',
+    description: 'frontend mentor challenge, HTML/CSS',
+    link: 'https://michaelbulaongdev.github.io/FM004-order-summary/'
+  },
+  {
+    image: 'https://i.imgur.com/4HXWPHD.png',
+    thumbnail: 'product preview thumbnail',
+    title: 'Product Preview',
+    description: 'frontend mentor challenge, HTML/CSS',
+    link: 'https://michaelbulaongdev.github.io/FM003-product-preview/'
+  },
+];
+
+export default listWorks;
