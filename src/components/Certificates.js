1a26ad8f1018950a886f9533412dc9483b8758e1
michaelbulaongdev <michaelbulaongdev@gmail.com>
2023-02-27 11:18:20 +0800

    Update Certificates.js
    tried incorporating accordion component to show certificate thumbnails

--- src/components/Certificates.js
+++ src/components/Certificates.js
@@ -1,4 +1,8 @@
 import {
+	Accordion,
+	AccordionSummary,
+	AccordionDetails,
+	CardMedia,
 	Container,
 	List,
 	ListItem,
@@ -8,22 +12,33 @@ import {
 	Typography,
 } from '@mui/material';
 import WorkspacePremiumIcon from '@mui/icons-material/WorkspacePremium';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import {listCert} from './lists';
 
 export default function Certificates() {
 	return (
-		<Container maxWidth='xs' align='center'>
+		<Container maxWidth='sm' align='center'>
 			<Typography variant='h4'>Certificates</Typography>
 			<List>
 				{listCert.map((item) => (
-					<ListItem key={item.link} disablePadding>
-						<ListItemButton target='_blank' href={item.link}>
-							<ListItemIcon>
-								<WorkspacePremiumIcon color='success' size='large' />
-							</ListItemIcon>
-							<ListItemText secondary={item.title} />
-						</ListItemButton>
-					</ListItem>
+					<Accordion>
+						<AccordionSummary
+							expandIcon={<ExpandMoreIcon />}
+							aria-controls='panel1a-content'
+							id='panel1a-header'>
+							<ListItem key={item.link} disablePadding>
+								<ListItemButton target='_blank' href={item.link}>
+									<ListItemIcon>
+										<WorkspacePremiumIcon color='success' size='large' />
+									</ListItemIcon>
+									<ListItemText secondary={item.title} />
+								</ListItemButton>
+							</ListItem>
+						</AccordionSummary>
+						<AccordionDetails>
+							<CardMedia sx={{minHeight: 400}} image={item.image} />
+						</AccordionDetails>
+					</Accordion>
 				))}
 			</List>
 		</Container>
