306a64f96e4c539af3e124d7d359f31ed41f570c
michaelbulaongdev <michaelbulaongdev@gmail.com>
2023-02-27 10:37:13 +0800

    Update lists.js

--- src/components/lists.js
+++ src/components/lists.js
@@ -48,22 +48,27 @@ export const listWorks = [
 export const listCert = [
 	{
 		title: 'Introduction to Front-End Development by Meta (Coursera)',
+		image: 'https://i.imgur.com/uJsWEPW.png',
 		link: 'https://www.coursera.org/account/accomplishments/certificate/C5N6GC75MW8G',
 	},
 	{
 		title: 'Programming with JavaScript by Meta (Coursera)',
+		image: 'https://i.imgur.com/qhsMAGY.png',
 		link: 'https://www.coursera.org/account/accomplishments/certificate/KSTB3NHYL7Z7',
 	},
 	{
 		title: 'JavaScript Loops and Conditionals by LinkedIn Learning',
+		image: 'https://i.imgur.com/4CYBZ6W.png',
 		link: 'https://www.linkedin.com/learning/certificates/7cfb8768f26a2ee45bcfbadb4d84b5f9e2764661824a500b75a98a418f9a2435',
 	},
 	{
 		title: 'React Basics by Meta (Coursera)',
+		image: 'https://i.imgur.com/NFWxSkp.png',
 		link: 'https://www.coursera.org/account/accomplishments/certificate/YVRFURJ6LV29',
 	},
 	{
 		title: 'React.js Code Challenges by LinkedIn Learning',
+		image: 'https://i.imgur.com/fKFbbG3.png',
 		link: 'https://www.linkedin.com/learning/certificates/aa938cabc1002f6f135d0a5f04e423963896f882c9fde4dd4e5642a3e08bd64f',
 	},
 ];
