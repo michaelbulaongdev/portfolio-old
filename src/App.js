4d37bf8897b1e5ed741fd11c0d659bb1c7ce313c
michaelbulaongdev <michaelbulaongdev@gmail.com>
2023-03-20 17:17:20 +0800

    disabled dark mode

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import {ThemeProvider, createTheme} from '@mui/material/styles';
-import {CssBaseline} from '@mui/material';
+// import {ThemeProvider, createTheme} from '@mui/material/styles';
+// import {CssBaseline} from '@mui/material';
 import DrawerAppBar from './components/DrawerAppBar';
 import Home from './components/Home';
 import About from './components/About';
@@ -7,24 +7,24 @@ import Works from './components/Works';
 import Certificates from './components/Certificates';
 import Contact from './components/Contact';
 
-const darkTheme = createTheme({
-	palette: {
-		mode: 'dark',
-	},
-});
+// const darkTheme = createTheme({
+// 	palette: {
+// 		mode: 'dark',
+// 	},
+// });
 
 export default function App() {
 	return (
 		<div>
-			<ThemeProvider theme={darkTheme}>
-				<CssBaseline />
-				<DrawerAppBar />
-				<Home />
-				<About />
-				<Works />
-				<Certificates />
-				<Contact />
-			</ThemeProvider>
+			{/* <ThemeProvider theme={darkTheme}>
+				<CssBaseline /> */}
+			<DrawerAppBar />
+			<Home />
+			<About />
+			<Works />
+			<Certificates />
+			<Contact />
+			{/* </ThemeProvider> */}
 		</div>
 	);
 }
