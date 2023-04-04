f1f5eebda4fce5a2cba0492f4ddf9c226df81366
michaelbulaongdev <michaelbulaongdev@gmail.com>
2023-04-05 07:39:06 +0800

    removed fade in section

--- src/components/Certificates.js
+++ src/components/Certificates.js
@@ -14,43 +14,40 @@ import WorkspacePremiumIcon from '@mui/icons-material/WorkspacePremium';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import {listCert} from './lists';
 import Spacer from './Spacer';
-import FadeInSection from './FadeInSection';
 
 export default function Certificates() {
 	return (
 		<section>
-			<FadeInSection>
-				<Spacer id='Certificates' />
-				<Container maxWidth='sm' align='center'>
-					<Typography variant='h3'>My Certificates</Typography>
-					<List>
-						{listCert.map((item) => (
-							<Accordion disableGutters='true'>
-								<AccordionSummary
-									expandIcon={<ExpandMoreIcon />}
-									aria-controls='panel1a-content'
-									id='panel1a-header'>
-									<ListItem key={item.link} disablePadding>
-										<ListItemIcon>
-											<WorkspacePremiumIcon color='success' size='large' />
-										</ListItemIcon>
-										<ListItemText secondary={item.title} />
-									</ListItem>
-								</AccordionSummary>
-								<AccordionDetails>
-									<a
-										className='cert-image'
-										target='_blank'
-										rel='noreferrer'
-										href={item.link}>
-										<CardMedia sx={{minHeight: 350}} image={item.image} />
-									</a>
-								</AccordionDetails>
-							</Accordion>
-						))}
-					</List>
-				</Container>
-			</FadeInSection>
+			<Spacer id='Certificates' />
+			<Container maxWidth='sm' align='center'>
+				<Typography variant='h3'>My Certificates</Typography>
+				<List>
+					{listCert.map((item) => (
+						<Accordion disableGutters='true'>
+							<AccordionSummary
+								expandIcon={<ExpandMoreIcon />}
+								aria-controls='panel1a-content'
+								id='panel1a-header'>
+								<ListItem key={item.link} disablePadding>
+									<ListItemIcon>
+										<WorkspacePremiumIcon color='success' size='large' />
+									</ListItemIcon>
+									<ListItemText secondary={item.title} />
+								</ListItem>
+							</AccordionSummary>
+							<AccordionDetails>
+								<a
+									className='cert-image'
+									target='_blank'
+									rel='noreferrer'
+									href={item.link}>
+									<CardMedia sx={{minHeight: 350}} image={item.image} />
+								</a>
+							</AccordionDetails>
+						</Accordion>
+					))}
+				</List>
+			</Container>
 		</section>
 	);
 }
